d62bb77229a55b739b7b6c8da4c6b0fd7eddd119
avfinanzasconproposito-dot <avfinanzasconproposito@gmail.com>
2026-08-15 19:17:58 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,22 @@
-const CACHE = 'av-finanzas-v1';
-const ARCHIVOS = ['/', '/index.html', '/manifest.json', '/logo.png'];
+const CACHE = 'av-finanzas-v3';
+const ARCHIVOS = ['/', '/index.html', '/manifest.json', '/logo.png', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
